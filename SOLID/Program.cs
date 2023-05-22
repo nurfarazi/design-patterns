c829c623dfa7d8fee749b2daf6bdda93495b7502
farazi <nur369188@gmail.com>
2023-05-22 13:51:43 +0600

    Iniit

---
+++ SOLID/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SOLID
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+          
+        }
+    }
+    
+}
+
